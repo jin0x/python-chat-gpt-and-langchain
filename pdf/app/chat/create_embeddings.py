2292f3955fca3f143e99e8405122b04e41d44fa9
John Leskas <john.leskas@gmail.com>
2024-04-07 13:49:40 +0300

    chore: wip

--- pdf/app/chat/create_embeddings.py
+++ pdf/app/chat/create_embeddings.py
@@ -1,6 +1,7 @@
+import os
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from app.chat.vector_stores.pinecone import vector_store
+# from app.chat.vector_stores.pinecone import vector_store
 
 def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     text_splitter = RecursiveCharacterTextSplitter(
@@ -11,4 +12,9 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     loader = PyPDFLoader(pdf_path)
     docs = loader.load_and_split(text_splitter)
 
-    vector_store.add_documents(docs)
+    print(os.getenv("PINECONE_API_KEY"))
+    print(os.getenv("PINECONE_ENV_NAME"))
+    print(os.getenv("PINECONE_INDEX_NAME"))
+    print(os.getenv("OPENAI_API_KEY"))
+
+    # vector_store.add_documents(docs)
